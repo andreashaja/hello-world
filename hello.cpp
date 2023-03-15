3bdc407020390edfe8035591e068f2d65c8aefeb
Andreas Haja <ah@andreashaja.com>
2023-03-15 16:40:59 +0100

    Gendern beachten

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hallo Welt!\n";
-    cout << "Moin an alle Mitstudenten!\nGruesse von Marek" << endl;
+    cout << "Moin an alle Mitstudierenden!\nGruesse von Marek" << endl;
     return 0;
 }
